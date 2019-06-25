a053abc0a420ef647c4df63368f6ee975614a95b
angel <angelsol299@hotmail.com>
2019-06-25 15:19:33 +0200

    sending messages

--- scripts/app.js
+++ scripts/app.js
@@ -1,6 +1,14 @@
 //dom queries
 
 const chatList = document.querySelector(".chat-list");
+const newChatForm = document.querySelector(".new-chat");
+
+//add a new chat
+newChateForm.addEventListener("submit", e => {
+  e.preventDefault();
+  const message = newChatForm.message.value.trim();
+  chatroom.addChat(message);
+});
 
 //class instances
 const chatUI = new ChatUI(chatList);
