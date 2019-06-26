b4085c77a7a368b715613e3088fddfd613624854
angel <angelsol299@hotmail.com>
2019-06-26 12:52:31 +0200

    switching chat rooms

--- scripts/app.js
+++ scripts/app.js
@@ -4,6 +4,7 @@ const chatList = document.querySelector(".chat-list");
 const newChatForm = document.querySelector(".new-chat");
 const newNameForm = document.querySelector(".new-name");
 const updateMssg = document.querySelector(".update-mssg");
+const rooms = document.querySelector(".chat-rooms");
 
 //add a new chat
 newChatForm.addEventListener("submit", e => {
@@ -27,9 +28,19 @@ newNameForm.addEventListener("submit", e => {
   updateMssg.innerText = `Your name was updated to ${newName}`;
   setTimeout(() => (updateMssg.innerText = ""), 2500);
 });
+
+//update chatrooms
+rooms.addEventListener("click", e => {
+  console.log(e);
+});
+
+//check if local storage has a name
+
+const username = localStorage.username ? localStorage.username : "anon";
+
 //class instances
 const chatUI = new ChatUI(chatList);
-const chatroom = new Chatroom("general", "shaun");
+const chatroom = new Chatroom("general", username);
 
 //get chats and render
 chatroom.getChats(data => chatUI.render(data));
